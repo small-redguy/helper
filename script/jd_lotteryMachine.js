9577cdf4f4aaacdc7ff46ce34609280deb42e8f5
small-redguy <57616411+small-redguy@users.noreply.github.com>
2021-05-11 10:18:33 +0800

    新增活动

--- script/jd_lotteryMachine.js
+++ script/jd_lotteryMachine.js
@@ -2,10 +2,11 @@ const STRSPLIT = "|";
 const needSum = false;            //是否需要显示汇总
 const printDetail = false;        //是否显示出参详情
 
-const appIdArr = ['1EFRRxA','1EFRQwA','1EFRTyg','1EFRXxg','1EFRWyg','1EFRWyw','1EFRZwA','1EFRZwQ','1EFRYwA','1EFRYxw']
-const homeDataFunPrefixArr = ['interact_template','interact_template','','','','','','','','','','','','','','','','interact_template','interact_template','']
-const collectScoreFunPrefixArr = ['','','','','','','','','','','','','','','','','','interact_template','interact_template','']
-const lotteryResultFunPrefixArr = ['','','','','','','','','','','','','','','','','','','interact_template','interact_template','interact_template']
+
+const appIdArr = ['1EFRRxA','1EFRQwA','1EFRTyg','1EFRXxg','1EFVRww','1EFRYxA','1EFRZwA','1EFRZwQ','1EFRYwA']
+const homeDataFunPrefixArr = ['interact_template','interact_template','','','','','','','','','','','','','','','','interact_template','interact_template']
+const collectScoreFunPrefixArr = ['','','','','','','','','','','','','','','','','','interact_template','interact_template']
+const lotteryResultFunPrefixArr = ['','','','','','interact_template','','','','','','','','','','','','','interact_template','interact_template']
 
 let merge = {}
 //IOS等用户直接用NobyDa的jd cookie
